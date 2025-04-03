45e69e4d5e2d55818bd38e93f41e65fc14ebf939
Marcelo Rizzo <marcelorizzo@outlook.com.br>
2025-04-03 18:44:46 -0300

    Removendo todos os arquivos

--- whaticket/backend/src/database/migrations/20231026022141-add-userId-to-tasks.ts
+++
@@ -1,1 +0,0 @@
-'use strict';module.exports={up:(queryInterface,Sequelize)=>{return queryInterface.addColumn('Tasks','userId',{type:Sequelize.INTEGER,references:{model:'Users',key:'id',},onUpdate:'CASCADE',onDelete:'SET NULL',});},down:(queryInterface,Sequelize)=>{return queryInterface.removeColumn('Tasks','userId');},};
